c242add87d6cc835e45fb65ed63512c366604bdd
Md-Anisujjaman <contact.anisujjaman@gmail.com>
2022-06-04 23:03:10 +0600

    This is a problem of hackerrank

--- Test/Class.py
+++ Test/Class.py
@@ -1,2 +1,16 @@
-anis = ['a', 'an', 'the']
-print(anis)
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+def add(a, b):
+    return a+b
+
+def sub(a, b):
+    return a-b
+
+def product(a,b):
+    return a*b
+
+print(add(a, b))
+print(sub(a, b))
+print(product(a, b))
